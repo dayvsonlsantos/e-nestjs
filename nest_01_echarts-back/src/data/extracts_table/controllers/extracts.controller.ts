c2ffecca6bbedf36885dc18556f364a20bf4f0b7
Dayvson Lima <dayvsonlsantos@gmail.com>
2023-11-06 20:59:56 -0300

    feat: data processing

--- nest_01_echarts-back/src/data/extracts_table/controllers/extracts.controller.ts
+++ nest_01_echarts-back/src/data/extracts_table/controllers/extracts.controller.ts
@@ -22,8 +22,16 @@ export class ExtractsController {
         const optionsArray = userOptions.split(',');
         const query = `
             SELECT 
-                count(e.${optionsArray[0]})     AS "Documentos processados",
-                e.${optionsArray[1]}            AS "Documentos"
+                count(e.${optionsArray[0]})                         AS "Documentos processados",
+                CASE
+                    WHEN e.${optionsArray[1]} = 'CNH' THEN UPPER(e.${optionsArray[1]})
+                    WHEN e.${optionsArray[1]} = 'POSICAO_CONSOLIDADA' THEN REPLACE(e.${optionsArray[1]}, 'POSICAO_CONSOLIDADA', 'Posição Consolidada')
+                    WHEN e.${optionsArray[1]} = 'FATURA_ENERGIA' THEN REPLACE(e.${optionsArray[1]}, 'FATURA_ENERGIA', 'Fatura de Energia')
+                    WHEN e.${optionsArray[1]} = 'DECLARACAO_IR' THEN REPLACE(e.${optionsArray[1]}, 'DECLARACAO_IR', 'Declaração de Imposto de Renda')
+                    WHEN e.${optionsArray[1]} = 'COMPROVANTE_RESIDENCIA' THEN REPLACE(e.${optionsArray[1]}, 'COMPROVANTE_RESIDENCIA', 'Comprovante de Residência')
+                    WHEN e.${optionsArray[1]} = 'BALANCO_PATRIMONIAL' THEN REPLACE(e.${optionsArray[1]}, 'BALANCO_PATRIMONIAL', 'Balanço Patrimonial')
+                    ELSE REPLACE(INITCAP(e.${optionsArray[1]}), '_', ' ')
+                END AS "Documentos"
             FROM extracts AS e
             GROUP BY e.${optionsArray[0]}
         `; // Construa a consulta SQL dinâmica aqui
