6b79569924c63d26a239d1dad188dfb69e537e0e
Dayvson Lima <dayvsonlsantos@gmail.com>
2023-11-08 07:55:42 -0300

    feat: Improving the code and creating more queries

--- nest_01_echarts-back/src/data/extracts_table/controllers/extracts.controller.ts
+++ nest_01_echarts-back/src/data/extracts_table/controllers/extracts.controller.ts
@@ -19,45 +19,57 @@ export class ExtractsController {
 
     @Get()
     async consultarOpcoes(@Query('userOptions') userOptions: string) {
-        const optionsArray = userOptions.split(',');
         let query: string = '';
-        let group: string = '';
-        if ((optionsArray[0] === 'pages_process') || (optionsArray[1] === 'pages_process')) {
-            if(optionsArray[0] === 'doc_type'){
-                group = optionsArray[0];
-            }else{
-                group = optionsArray[1];
-            }
+
+        const hasOtherValue = userOptions.split(',').some(option => option !== 'doc_type');
+
+        console.log('test'+hasOtherValue)
+
+        if (userOptions.includes('pages_process') && userOptions.includes('doc_type')) {
             query = `
                 SELECT 
-                    sum(e.${optionsArray[0]})                         AS "Páginas Processadas",
+                    sum(e.pages_process)                         AS "Páginas Processadas",
                     CASE
-                        WHEN e.${optionsArray[1]} = 'CNH' THEN UPPER(e.${optionsArray[1]})
-                        WHEN e.${optionsArray[1]} = 'POSICAO_CONSOLIDADA' THEN REPLACE(e.${optionsArray[1]}, 'POSICAO_CONSOLIDADA', 'Posição Consolidada')
-                        WHEN e.${optionsArray[1]} = 'FATURA_ENERGIA' THEN REPLACE(e.${optionsArray[1]}, 'FATURA_ENERGIA', 'Fatura de Energia')
-                        WHEN e.${optionsArray[1]} = 'DECLARACAO_IR' THEN REPLACE(e.${optionsArray[1]}, 'DECLARACAO_IR', 'Declaração de Imposto de Renda')
-                        WHEN e.${optionsArray[1]} = 'COMPROVANTE_RESIDENCIA' THEN REPLACE(e.${optionsArray[1]}, 'COMPROVANTE_RESIDENCIA', 'Comprovante de Residência')
-                        WHEN e.${optionsArray[1]} = 'BALANCO_PATRIMONIAL' THEN REPLACE(e.${optionsArray[1]}, 'BALANCO_PATRIMONIAL', 'Balanço Patrimonial')
-                        ELSE REPLACE(INITCAP(e.${optionsArray[1]}), '_', ' ')
+                        WHEN e.doc_type = 'CNH' THEN UPPER(e.doc_type)
+                        WHEN e.doc_type = 'POSICAO_CONSOLIDADA' THEN REPLACE(e.doc_type, 'POSICAO_CONSOLIDADA', 'Posição Consolidada')
+                        WHEN e.doc_type = 'FATURA_ENERGIA' THEN REPLACE(e.doc_type, 'FATURA_ENERGIA', 'Fatura de Energia')
+                        WHEN e.doc_type = 'DECLARACAO_IR' THEN REPLACE(e.doc_type, 'DECLARACAO_IR', 'Declaração de Imposto de Renda')
+                        WHEN e.doc_type = 'COMPROVANTE_RESIDENCIA' THEN REPLACE(e.doc_type, 'COMPROVANTE_RESIDENCIA', 'Comprovante de Residência')
+                        WHEN e.doc_type = 'BALANCO_PATRIMONIAL' THEN REPLACE(e.doc_type, 'BALANCO_PATRIMONIAL', 'Balanço Patrimonial')
+                        ELSE REPLACE(INITCAP(e.doc_type), '_', ' ')
                     END AS "Tipo de Documento"
                 FROM extracts AS e
-                GROUP BY e.${group}
+                GROUP BY e.doc_type
             `;
-        } else if ((optionsArray[0] === 'doc_type') || (optionsArray[1] === 'doc_type')) {
+        }  else if (userOptions.includes('pages_process') && userOptions.includes('name')) {
             query = `
                 SELECT 
-                    count(e.${optionsArray[0]})                         AS "Documentos processados",
+                    count(e.doc_type)       AS "Quantidade de documentos processados",
+                    u.name                  AS "Usuário"
+                FROM 
+                    extracts AS e
+                JOIN
+                    users AS u
+                ON
+                    u.id = e.user_id
+                GROUP BY
+                    e.doc_type
+            `
+        }else if (hasOtherValue === false){
+            query = `
+                SELECT 
+                    count(e.doc_type)                         AS "Documentos processados",
                     CASE
-                        WHEN e.${optionsArray[1]} = 'CNH' THEN UPPER(e.${optionsArray[1]})
-                        WHEN e.${optionsArray[1]} = 'POSICAO_CONSOLIDADA' THEN REPLACE(e.${optionsArray[1]}, 'POSICAO_CONSOLIDADA', 'Posição Consolidada')
-                        WHEN e.${optionsArray[1]} = 'FATURA_ENERGIA' THEN REPLACE(e.${optionsArray[1]}, 'FATURA_ENERGIA', 'Fatura de Energia')
-                        WHEN e.${optionsArray[1]} = 'DECLARACAO_IR' THEN REPLACE(e.${optionsArray[1]}, 'DECLARACAO_IR', 'Declaração de Imposto de Renda')
-                        WHEN e.${optionsArray[1]} = 'COMPROVANTE_RESIDENCIA' THEN REPLACE(e.${optionsArray[1]}, 'COMPROVANTE_RESIDENCIA', 'Comprovante de Residência')
-                        WHEN e.${optionsArray[1]} = 'BALANCO_PATRIMONIAL' THEN REPLACE(e.${optionsArray[1]}, 'BALANCO_PATRIMONIAL', 'Balanço Patrimonial')
-                        ELSE REPLACE(INITCAP(e.${optionsArray[1]}), '_', ' ')
+                        WHEN e.doc_type = 'CNH' THEN UPPER(e.doc_type)
+                        WHEN e.doc_type = 'POSICAO_CONSOLIDADA' THEN REPLACE(e.doc_type, 'POSICAO_CONSOLIDADA', 'Posição Consolidada')
+                        WHEN e.doc_type = 'FATURA_ENERGIA' THEN REPLACE(e.doc_type, 'FATURA_ENERGIA', 'Fatura de Energia')
+                        WHEN e.doc_type = 'DECLARACAO_IR' THEN REPLACE(e.doc_type, 'DECLARACAO_IR', 'Declaração de Imposto de Renda')
+                        WHEN e.doc_type = 'COMPROVANTE_RESIDENCIA' THEN REPLACE(e.doc_type, 'COMPROVANTE_RESIDENCIA', 'Comprovante de Residência')
+                        WHEN e.doc_type = 'BALANCO_PATRIMONIAL' THEN REPLACE(e.doc_type, 'BALANCO_PATRIMONIAL', 'Balanço Patrimonial')
+                        ELSE REPLACE(INITCAP(e.doc_type), '_', ' ')
                     END AS "Tipo de Documento"
                 FROM extracts AS e
-                GROUP BY e.${optionsArray[0]}
+                GROUP BY e.doc_type
             `;
         }
         // Construa a consulta SQL dinâmica aqui
