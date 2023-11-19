854bbfea31c54171560a490d9f17be4cec7e67f0
Dayvson Lima <dayvsonlsantos@gmail.com>
2023-11-18 22:32:20 -0300

    feat: Simplifying the code.

--- nest_01_echarts-back/src/data/extracts_table/controllers/extracts.controller.ts
+++ nest_01_echarts-back/src/data/extracts_table/controllers/extracts.controller.ts
@@ -1,10 +1,7 @@
-import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { ExtractsService } from '../services/extracts.service';
-import { Extracts } from '../models/extracts.interface';
-import { Observable } from 'rxjs';
-import { getRepository } from 'typeorm';
 
-@Controller('extracts')
+@Controller('data')
 export class ExtractsController {
     constructor(private extractsService: ExtractsService) { }
 
@@ -19,51 +16,15 @@ export class ExtractsController {
     // }
 
     @Get()
-    async consultarOpcoes(@Query('userOptions') userOptions: string, @Query('filterDate') filterDate: string, @Query('filterUserOptions') filterUserOptions: string) {
-
-        console.log('testenovo' + filterUserOptions)
+    async consultarOpcoes(@Query() userOptions: any) {
+        console.log('testenovo', userOptions);
 
         let query: string = '';
 
-        //(en) Checks if there is a value other than doc_type.
-        const hasOtherValue = userOptions.split(',').some(option => option !== 'doc_type');
-
-        //(en) Gets the current date.
-        const currentDate = new Date();
-
-        //(en) Converts the current date to a string.
-        const formattedDate = currentDate.toISOString();
-
-        //(en) Takes the date provided by the user and puts it in an array.
-        const filterDateArray = filterDate.split(',');
-
-        //(en) Separates the start and end dates.
-        let startDateFormated = new Date(filterDateArray[0]);
-        let endDateFormated = new Date(filterDateArray[1]);
-
-        //(en) Variables that will be used in the database queries.
-        let startDate: string = '';
-        let endDate: string = '';
-
-        //(en) If the user does not provide a date.
-        if (filterDate === '') {
-            //(en) Sets the start date as 2000/01/01. 
-            startDate = '2000-01-01T00:00:00.000Z';
-            //(en) Sets the end date as the current date.
-            endDate = formattedDate;
-        } else {
-            //(en) Sets the dates with the values provided by the user, converting them to strings.
-            startDate = startDateFormated.toISOString();
-            endDate = endDateFormated.toISOString();
-        }
-
-        //(en) Takes the filter options provided by the user and puts it in an array.
-        const filterUserOptionsArray = filterUserOptions.split(',');
-        console.log(userOptions.includes('pages_process') && userOptions.includes('segment') && filterUserOptionsArray[0] === 'avg')
-        console.log(filterUserOptionsArray[0] === 'avg')
+        userOptions.selectedOptions = userOptions.selectedOptions.split(',');
 
         try {
-            if (userOptions.includes('pages_process') && userOptions.includes('doc_type') && filterUserOptionsArray[0] === '') {
+            if (userOptions.selectedOptions.includes('pages_process') && userOptions.selectedOptions.includes('doc_type') && userOptions.aggregate === 'sum') {
                 query = `
                     SELECT 
                         sum(e.pages_process)                         AS "Páginas Processadas",
@@ -77,10 +38,10 @@ export class ExtractsController {
                             ELSE REPLACE(INITCAP(e.doc_type), '_', ' ')
                         END AS "Tipo de Documento"
                     FROM extracts AS e
-                    WHERE e.created_at >= '${startDate}' AND e.created_at <= '${endDate}'
+                    WHERE e.created_at >= '${userOptions.startDate}' AND e.created_at <= '${userOptions.endDate}'
                     GROUP BY e.doc_type
                 `;
-            } else if (userOptions.includes('doc_count') && userOptions.includes('name') && filterUserOptionsArray[0] === '') {
+            } else if (userOptions.selectedOptions.includes('doc_count') && userOptions.selectedOptions.includes('name') && userOptions.aggregate === '') {
                 query = `
                     SELECT 
                         count(e.doc_type)       AS "Documentos processados",
@@ -91,11 +52,11 @@ export class ExtractsController {
                         users AS u
                     ON
                         u.id = e.user_id
-                    WHERE e.created_at >= '${startDate}' AND e.created_at <= '${endDate}'
+                    WHERE e.created_at >= '${userOptions.startDate}' AND e.created_at <= '${userOptions.endDate}}'
                     GROUP BY
                         u.name
                 `
-            } else if (userOptions.includes('pages_process') && userOptions.includes('name') && filterUserOptionsArray[0] === '') {
+            } else if (userOptions.selectedOptions.includes('pages_process') && userOptions.selectedOptions.includes('name') && userOptions.aggregate === 'sum') {
                 query = `
                     SELECT 
                         sum(e.pages_process)        AS "Páginas Processadas",
@@ -106,11 +67,11 @@ export class ExtractsController {
                         users AS u
                     ON
                         u.id = e.user_id
-                    WHERE e.created_at >= '${startDate}' AND e.created_at <= '${endDate}'
+                    WHERE e.created_at >= '${userOptions.startDate}' AND e.created_at <= '${userOptions.endDate}}'
                     GROUP BY
                         u.name
                 `
-            } else if (userOptions.includes('pages_process') && userOptions.includes('segment') && filterUserOptionsArray[0] === '') {
+            } else if (userOptions.selectedOptions.includes('pages_process') && userOptions.selectedOptions.includes('segment') && userOptions.aggregate === 'sum') {
                 query = `
                     SELECT 
                         sum(e.pages_process)        AS "Páginas Processadas",
@@ -126,11 +87,11 @@ export class ExtractsController {
                         users AS u
                     ON
                         u.id = e.user_id
-                    WHERE e.created_at >= '${startDate}' AND e.created_at <= '${endDate}'
+                    WHERE e.created_at >= '${userOptions.startDate}' AND e.created_at <= '${userOptions.endDate}}'
                     GROUP BY
                         u.segment
                 `
-            } else if (userOptions.includes('doc_count') && userOptions.includes('segment') && filterUserOptionsArray[0] === '') {
+            } else if (userOptions.selectedOptions.includes('doc_count') && userOptions.selectedOptions.includes('segment') && userOptions.aggregate === '') {
                 query = `
                     SELECT 
                         count(e.doc_type)        AS "Documentos processados",
@@ -146,11 +107,11 @@ export class ExtractsController {
                         users AS u
                     ON
                         u.id = e.user_id
-                    WHERE e.created_at >= '${startDate}' AND e.created_at <= '${endDate}'
+                    WHERE e.created_at >= '${userOptions.startDate}' AND e.created_at <= '${userOptions.endDate}}'
                     GROUP BY
                         u.segment
                 `
-            } else if (userOptions.includes('created_at') && userOptions.includes('pages_process') && filterUserOptionsArray[0] === '') {
+            } else if (userOptions.selectedOptions.includes('created_at') && userOptions.selectedOptions.includes('pages_process') && userOptions.aggregate === 'sum') {
                 query = `
                     SELECT 
                         TO_CHAR(DATE_TRUNC('day', created_at), 'DD/MM/YYYY') AS "Data de Criação",
@@ -159,13 +120,13 @@ export class ExtractsController {
                     FROM 
                         extracts AS e
                     WHERE 
-                        e.created_at >= '${startDate}' AND e.created_at <= '${endDate}'
+                        e.created_at >= '${userOptions.startDate}' AND e.created_at <= '${userOptions.endDate}}'
                     GROUP BY 
                         e.created_at
                     ORDER BY 
                         e.created_at
                 `
-            } else if (userOptions.includes('pages_process') && userOptions.includes('doc_type') && filterUserOptionsArray[0] === 'avg') {
+            } else if (userOptions.selectedOptions.includes('pages_process') && userOptions.selectedOptions.includes('doc_type') && userOptions.aggregate === 'avg') {
                 query = `
                     SELECT 
                         ROUND(avg(e.pages_process),2)        AS "Páginas Processadas",
@@ -180,11 +141,11 @@ export class ExtractsController {
                         END AS "Tipo de Documento"
                     FROM 
                         extracts AS e
-                    WHERE e.created_at >= '${startDate}' AND e.created_at <= '${endDate}'
+                    WHERE e.created_at >= '${userOptions.startDate}' AND e.created_at <= '${userOptions.endDate}}'
                     GROUP BY
                         e.doc_type
                 `
-            } else if (userOptions.includes('pages_process') && userOptions.includes('segment') && filterUserOptionsArray[0] === 'avg') {
+            } else if (userOptions.selectedOptions.includes('pages_process') && userOptions.selectedOptions.includes('segment') && userOptions.aggregate === 'avg') {
                 query = `
                     SELECT 
                         ROUND(avg(e.pages_process),2)        AS "Páginas Processadas",
@@ -200,11 +161,11 @@ export class ExtractsController {
                         users AS u
                     ON
                         u.id = e.user_id
-                    WHERE e.created_at >= '${startDate}' AND e.created_at <= '${endDate}'
+                    WHERE e.created_at >= '${userOptions.startDate}' AND e.created_at <= '${userOptions.endDate}}'
                     GROUP BY
                         u.segment
                 `
-            } else if (hasOtherValue === false) {
+            } else if (userOptions.selectedOptions.includes('doc_type') && userOptions.selectedOptions.includes('doc_count') && userOptions.aggregate === 'avg') {
                 query = `
                     SELECT 
                         count(e.doc_type)                         AS "Documentos processados",
@@ -218,11 +179,10 @@ export class ExtractsController {
                             ELSE REPLACE(INITCAP(e.doc_type), '_', ' ')
                         END AS "Tipo de Documento"
                     FROM extracts AS e
-                    WHERE e.created_at >= '${startDate}' AND e.created_at <= '${endDate}'
+                    WHERE e.created_at >= '${userOptions.startDate}' AND e.created_at <= '${userOptions.endDate}}'
                     GROUP BY e.doc_type
-                `;
+                `
             }
-
             return this.extractsService.executarConsulta(query);
         } catch (error) {
             console.error('Erro ao executar consulta:', error);
---
+++ nest_01_echarts-back/src/interfaces/user-options.ts
@@ -0,0 +1,8 @@
+export interface UserOptions {
+    chartType: string,
+    selectedOptions: string[],
+    startDate: string,
+    endDate: string,
+    aggregate: string,
+    timeGrouping: string
+}
